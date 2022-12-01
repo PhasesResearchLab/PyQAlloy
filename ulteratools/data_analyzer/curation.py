151bebc7549752ad8d2c847efde58b59999e82cd
Adam <ak@psu.edu>
2022-12-01 17:35:06 -0500

    - moved getCompVecs back into the anaylzer class, as future functions require different customizations of the function - a few variables are now class variables

--- ulteratools/data_analyzer/curation.py
+++ ulteratools/data_analyzer/curation.py
@@ -6,23 +6,6 @@ from pymatgen.core import Composition
 from sklearn.neighbors import NearestNeighbors
 
 
-def getCompVecs(collection: Collection, doi: str):
-    formulas, els, names, compVecs = set(), set(), set(), list()
-    # Find a set of unique formulas from DOI and a set of all elements present in them
-    for e in collection.find({'reference.doi': doi}):
-        c = Composition(e['material']['formula'])
-        formulas.add(c.reduced_formula)
-        names.add(e['meta']['name'])
-        els.update(list(c.get_el_amt_dict().keys()))
-    # Vectorize based on a list of elements
-    els = list(els)
-    for f in formulas:
-        cd = dict(Composition(f).fractional_composition.get_el_amt_dict())
-        compVec = [cd[el] if el in cd else 0 for el in els]
-        compVecs.append(compVec)
-
-    return compVecs, formulas, names, els
-
 class Analyzer:
     def __init__(self, database, collection):
         with resources.files('ulteratools').joinpath('credentials.json').open('r') as f:
@@ -34,19 +17,26 @@ class Analyzer:
         print(f'Connected to the {collection} in {database} with {self.collection.estimated_document_count()} data '
               f'points detected.')
 
+    def get_allDOIs(self):
+        return [e['doi'] for e in self.collection.aggregate([
+            {'$match': {'reference.doi': {'$ne': None}}},
+            {'$group': {'_id': '$reference.doi'}},
+            {'$set': {'doi': '$_id', '_id': '$$REMOVE'}}])]
+
 class SingleDOIAnalyzer(Analyzer):
 
     def __init__(self, doi=None, name=None, database='ULTERA', collection='CURATED'):
         super().__init__(database=database, collection=collection)
         self.name = name
         self.doi = doi
-        self.formulas = None
-        self.nn_distances = None
-        self.names = None
-        self.els = None
-        self.compVecs = None
+        self.formulas = set()
+        self.nn_distances = list()
+        self.names = set()
+        self.els = set()
+        self.compVecs = list()
+        self.fStrings = list()
+        print(f'********  Analyzer Initialized  ********')
 
-        print(f'********  AbnormalDataAnalyzer Initialized  ********')
 
     def setDOI(self, doi: str):
         self.doi = doi
@@ -54,15 +44,30 @@ class SingleDOIAnalyzer(Analyzer):
     def setName(self, name):
         self.name = name
 
-    def get_allDOIs(self):
-        return [e['doi'] for e in self.collection.aggregate([
-            {'$match': {'reference.doi': {'$ne': None}}},
-            {'$group': {'_id': '$reference.doi'}},
-            {'$set': {'doi': '$_id', '_id': '$$REMOVE'}}])]
+    def getCompVecs(self, collection: Collection, doi: str):
+        # Reset formulas, els, etc
+        self.formulas, self.els, self.names, self.compVecs, self.fStrings = set(), set(), set(), list(), list()
+        # Find a set of unique formulas from DOI and a set of all elements present in them
+        for e in collection.find({'reference.doi': doi}):
+            c = Composition(e['material']['formula'])
+            self.formulas.add(c.reduced_formula)
+            self.names.add(e['meta']['name'])
+            self.els.update(list(c.get_el_amt_dict().keys()))
+            self.fStrings.append(
+                e['material']['formula']#+'<br>'+
+                #e['material']['percentileFormula']+'<br>'+
+                #e['material']['relationalFormula']
+                )
+        # Vectorize based on a list of elements
+        self.els = list(self.els)
+        for f in self.formulas:
+            cd = dict(Composition(f).fractional_composition.get_el_amt_dict())
+            compVec = [cd[el] if el in cd else 0 for el in self.els]
+            self.compVecs.append(compVec)
 
     def analyze_nnDistances(self):
         nn = NearestNeighbors(n_neighbors=2, metric='l1', algorithm='kd_tree')
-        self.compVecs, self.formulas, self.names, self.els = getCompVecs(collection=self.collection, doi=self.doi)
+        self.getCompVecs(collection=self.collection, doi=self.doi)
         self.nn_distances = [l[1] for l in nn.fit(self.compVecs).kneighbors(self.compVecs)[0]]
 
     def print_nnDistances(self):
@@ -75,4 +80,6 @@ class SingleDOIAnalyzer(Analyzer):
                 print(f'{round(l, 4):<10}|  {round(l/maxD, 4):<10} <-- {f}')
         else:
             print(f'Skipping {self.doi}. Specified researcher ({self.name}) not present in the group ({self.names})')
+
+
 
