7c5face9a49298da3a9cfc6c570c1c60e941873c
Adam <ak@psu.edu>
2022-12-01 12:32:19 -0500

    - test for Single DOI NN analysis

--- ulteratools/tests/test_AbnormalDataAnalyzer.py
+++ ulteratools/tests/test_AbnormalDataAnalyzer.py
@@ -5,17 +5,32 @@ from ulteratools.data_analyzer import curation
 class TestADA(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.ADA = curation.AbnormalDataAnalyzer()
+        doi = '10.3390/met9010076'
+        self.sDOI = curation.SingleDOIAnalyzer(doi=doi)
         pass
 
     def test_DOIAggregation(self):
-        doiList = self.ADA.get_allDOIs()
+        doiList = self.sDOI.get_allDOIs()
         print(f'{len(doiList)} DOIs detected in the database')
         self.assertGreater(len(doiList), 0)
 
     def test_NNdistance(self):
-        doi = '10.3390/met9010076'
-        self.ADA.analyze_nnDistances(doi)
+        with self.subTest(msg='Find Distances'):
+            self.sDOI.analyze_nnDistances()
+
+        with self.subTest(msg='Test Printout'):
+            self.sDOI.print_nnDistances()
+
+        with self.subTest(msg='Test Name Selection. - filled'):
+            temp_sDOI = curation.SingleDOIAnalyzer(name='Marcia Ahn', doi='10.3390/met9010076')
+            temp_sDOI.analyze_nnDistances()
+            temp_sDOI.print_nnDistances()
+
+        with self.subTest(msg='Test Name Selection. - empty'):
+            temp_sDOI = curation.SingleDOIAnalyzer(name='wnjvbwejhcv', doi='10.3390/met9010076')
+            temp_sDOI.analyze_nnDistances()
+            temp_sDOI.print_nnDistances()
+
 
 if __name__ == '__main__':
     unittest.main()
