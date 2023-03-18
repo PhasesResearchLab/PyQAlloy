24c335a460302afcf980790fbf6effa3913d7156
amkrajewski <ak@psu.edu>
2023-03-18 10:53:50 -0400

    - tests for initialization of the All Data Analyzer and TSNE embedding routine - NOTE: ULTERA is dynamic, so no exact outcome values are tested here, just whether they succesfully complete

--- pyqalloy/tests/test_AllDataAnalyzer.py
+++ pyqalloy/tests/test_AllDataAnalyzer.py
@@ -2,11 +2,33 @@ import unittest
 
 from pyqalloy.data_analyzer import curation
 
-class TestAllDataContext(unittest.TestCase):
+class TestAllDataContextULTERA(unittest.TestCase):
 
     def setUp(self) -> None:
         self.allD = curation.AllDataAnalyzer()
-        pass
+
+    def testInit(self) -> None:
+        self.assertGreater(len(self.allD.allComps), 0)
+        self.assertIn('formula', self.allD.allComps[0])
+        self.assertIn('compVec', self.allD.allComps[0])
+
+    def testTSNE(self) -> None:
+        with self.subTest(msg='Find TSNE Embedding under large perplexity'):
+            self.allD.getTSNE(perplexity=30)
+
+            self.assertIn('compVec_TSNE2D', self.allD.allComps[0])
+            self.assertEqual(len(self.allD.allComps[0]['compVec_TSNE2D']), 2)
+
+        with self.subTest(msg='Find TSNE Embedding under low (stnadard) perplexity'):
+            self.allD.getTSNE()
+
+            self.assertIn('compVec_TSNE2D', self.allD.allComps[0])
+            self.assertEqual(len(self.allD.allComps[0]['compVec_TSNE2D']), 2)
+
+        with self.subTest(msg='Test if the TSNE embedding successfully prints out'):
+            self.allD.showTSNE()
+
+
 
 
 
