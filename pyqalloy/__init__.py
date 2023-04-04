9dbf6e7ea1ba06020ccbaf2119668b3054aba40e
amkrajewski <ak@psu.edu>
2023-04-04 10:15:55 -0400

    - updated docs for showDocs function

--- pyqalloy/__init__.py
+++ pyqalloy/__init__.py
@@ -2,9 +2,13 @@ from pyqalloy.core import *
 from pyqalloy.curation import *
 
 def showDocs():
-    """Open the offline documentation in a web browser."""
+    """Open the offline documentation in a web browser, if the documentation is available locally, i.e. when you are
+    in the cloned pySIPFENN GitHub repository you've installed in editable mode. Note the function doesn't use importlib
+    since docs are not part of the package for space savings. Otherwise, the function opens the online documentation."""
+
     import os
     if os.path.isfile('docs/_build/index.html'):
+        print('Found the loacal documentation. Opening it now...')
         os.system('open docs/_build/index.html')
     else:
         os.system('open https://ultera.org')
