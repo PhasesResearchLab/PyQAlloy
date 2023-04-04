4f0219a6aa74f916c1d601c9cf4f89b8602ffc3f
amkrajewski <ak@psu.edu>
2023-04-04 11:31:07 -0400

    - added new test to SingleCompositionAnalyzer verifying bounds are working as expected (partially)

--- pyqalloy/tests/test_SingleCompositionAnalyzer.py
+++ pyqalloy/tests/test_SingleCompositionAnalyzer.py
@@ -28,6 +28,17 @@ class TestSCADA(unittest.TestCase):
                            resLen1,
                            msg='The number of results should increase when the query limit is increased')
 
+    def test_ScanBounds(self):
+        self.sC.scanCompositionsAround100(queryLimit=1000, lowerBound=95, upperBound=105, printOnFly=False)
+        resLen1 = len(self.sC.printOuts)
+        print(f'First scan (LB=95, UB=105): {resLen1} results')
+        self.sC = analysis.SingleCompositionAnalyzer()
+        self.sC.scanCompositionsAround100(queryLimit=1000, lowerBound=50, upperBound=150, printOnFly=False)
+        resLen2 = len(self.sC.printOuts)
+        print(f'Second scan (LB=50, UB=150): {resLen2} results')
+        self.assertGreater(resLen2, resLen1, msg='The number of results should increase when the bounds of what is '
+                                                 'considered "around 100%" are increased from 95-105% to 50-150%')
+
     def test_ScanRL(self):
         with self.subTest(msg='Scan'):
             self.sC.scanCompositionsAround100(resultLimit=5, printOnFly=True)
