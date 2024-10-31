87808040cd3a673a35880b3d34a535b47724f0dc
Adam M. Krajewski <54290107+amkrajewski@users.noreply.github.com>
2024-10-31 16:05:25 -0400

    (tests) imporved existing single DOI tests; improved reference data comparisons; general improvements

--- pyqalloy/tests/test_SingleDOIAnalyzer.py
+++ pyqalloy/tests/test_SingleDOIAnalyzer.py
@@ -2,6 +2,12 @@ import unittest
 
 from pyqalloy.curation import analysis
 
+referenceDOIs = ['10.1557/adv.2017.76', '10.1557/jmr.2019.18', '10.1557/jmr.2019.36', '10.1557/jmr.2019.40', 
+                 '10.3390/coatings9010016', '10.3390/e16020870', '10.3390/e18050189', '10.3390/e21010015', 
+                 '10.3390/e21020114', '10.3390/e21020122', '10.3390/e21020169', '10.3390/e21030288']
+
+nnDistancesRef = [0.08658008658008651, 0.0909090909090909, 0.08225108225108217, 0.09090909090909093, 0.08225108225108217]
+
 class TestADA(unittest.TestCase):
 
     def setUp(self) -> None:
@@ -14,14 +20,35 @@ class TestADA(unittest.TestCase):
     def test_DOIAggregation(self):
         doiList = self.sDOI.get_allDOIs()
         print(f'{len(doiList)} DOIs detected in the database')
-        self.assertGreater(len(doiList), 0)
+        self.assertGreater(len(doiList), 0, msg='No DOIs detected in the database')
+
+        for doi in referenceDOIs:
+            with self.subTest(msg=f'Test {doi}'):
+                self.assertIn(doi, doiList, msg=f'Known DOI {doi} not found in the list of DOIs')
+
+        self.assertEqual(
+            len(doiList), 479, 
+            msg='Incorrect number of DOIs found. Make sure you are using CURATED_Dec2022 snapshot or adjust the reference number of DOIs.')
+
 
     def test_NNdistance(self):
         with self.subTest(msg='Find Distances'):
             self.sDOI.analyze_nnDistances()
 
+            for i, dist in enumerate(nnDistancesRef):
+                with self.subTest(msg=f'Test {i}'):
+                    self.assertAlmostEqual(dist, self.sDOI.nn_distances[i], places=5, msg=f'Incorrect distance found for {i}th nearest neighbor (in the reference: {self.sDOI.doi})')
+
         with self.subTest(msg='Test Printout'):
-            self.sDOI.print_nnDistances()
+            self.sDOI.print_nnDistances(printOut=False)
+
+            self.assertIn('10.1016/j.jallcom.2008.11.059', self.sDOI.printLog, msg='Printout does not contain the DOI')
+            self.assertIn('Adam Krajewski', self.sDOI.printLog, msg='Printout does not contain the name')
+            self.assertIn(
+                '0.0866    |  0.9524     <-- F: Ti0.5 Cr1 Fe1 Co1 Ni1 Cu0.5 Al0.5   | PF: Ti9.1 Cr18.2 Fe18.2 Co18.2 Ni18.2 Cu9.1 Al9.1  | Raw: Al0.5CoCrCu0.5FeNiTi0.5   | RF: Ti1 Cr2 Fe2 Co2 Ni2 Cu1 Al1', 
+                self.sDOI.printLog,
+                msg='Printout does not contain a reference result string. Was any printout style changed?'
+                )
 
         with self.subTest(msg='Test Name Selection. - filled'):
             self.sDOI.setName('Adam Krajewski')
@@ -52,6 +79,10 @@ class TestADA(unittest.TestCase):
         with self.subTest(msg='Get PCA'):
             self.sDOI.getCompVecs()
             self.sDOI.get_compVecs_2DPCA()
+
+            self.assertGreater(len(self.sDOI.compVecs), 0, msg='PCA data not generated')
+            self.assertEqual(len(self.sDOI.compVecs_2DPCA[0]), 2, msg='Incorrect dimensionality of the PCA result')
+            self.assertEqual(len(self.sDOI.compVecs_2DPCA), 5, msg='Incorrect number of entries in the PCA result')
 
         with self.subTest(msg='Analyze the Found PCA'):
             self.sDOI.analyze_compVecs_2DPCA(showFigure=False)
